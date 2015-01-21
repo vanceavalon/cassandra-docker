181b4fff01eae8bfe18bc099258225a031f52242
Chris Goller <goller@gmail.com>
2015-01-20 22:50:54 -0600

    Update opscenter config to use port 2222 for node ssh

--- opscenter/src/start.sh
+++ opscenter/src/start.sh
@@ -3,6 +3,8 @@
 IP=`hostname --ip-address`
 
 sed -i -e "s/^interface.*/interface = $IP/" /etc/opscenter/opscenterd.conf
+echo "[agents]" >> /etc/opscenter/opscenterd.conf
+echo "ssh_port = 2222"  >> /etc/opscenter/opscenterd.conf
 
 echo Starting OpsCenter on $IP...
 service opscenterd start
